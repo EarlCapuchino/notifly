e3071d49bff30ee2ecefcb6bbf6f9e4f27f5edd7
Earl Capuchino <70846033+EarlCapuchino@users.noreply.github.com>
2023-04-17 14:38:03 +0800

    Update sidebar.js

--- client/src/fakeDb/sidebar.js
+++ client/src/fakeDb/sidebar.js
@@ -5,9 +5,87 @@ const Sidebar = [
     path: "dashboard",
   },
   {
+    name: "Messaging",
+    icon: "mail-bulk",
+    path: "messaging",
+  },
+  {
+    name: "Tag people",
+    icon: "tag",
+    path: "tags",
+  },
+  {
     name: "Cluster",
     icon: "object-group",
     path: "clusters",
+    children: [
+      {
+        name: "List",
+        icon: "clipboard-list",
+        path: "list",
+      },
+      {
+        name: "Archive",
+        icon: "folder-minus",
+        path: "archive",
+      },
+    ],
+  },
+  {
+    name: "Members",
+    icon: "users",
+    path: "members",
+    children: [
+      {
+        name: "List",
+        icon: "clipboard-list",
+        path: "list",
+      },
+      {
+        name: "Archive",
+        icon: "folder-minus",
+        path: "archive",
+      },
+    ],
+  },
+  {
+    name: "Meetings",
+    icon: "calendar-alt",
+    path: "meetings",
+    children: [
+      {
+        name: "List",
+        icon: "clipboard-list",
+        path: "list",
+      },
+      {
+        name: "Archive",
+        icon: "folder-minus",
+        path: "archive",
+      },
+    ],
+  },
+  {
+    name: "Posts",
+    icon: "newspaper",
+    path: "posts",
+    children: [
+      {
+        name: "List",
+        icon: "clipboard-list",
+        path: "list",
+      },
+      {
+        name: "Archive",
+        icon: "folder-minus",
+        path: "archive",
+      },
+    ],
+  },
+  {
+    name: "Pages",
+    icon: "book-open",
+    path: "pages",
     children: [
       {
         name: "List",

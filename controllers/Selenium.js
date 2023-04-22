086d1fbbfc6a8110395096b3885d6ff41a2785f7
Earl Capuchino <70846033+EarlCapuchino@users.noreply.github.com>
2023-04-22 15:24:51 +0800

    sharing complete

--- controllers/Selenium.js
+++ controllers/Selenium.js
@@ -18,6 +18,13 @@ exports.messages = async (req, res) => {
   // If login was successful
   if (status) {
     console.log(">>selenium/messages - fb login success");
+
+    var response = {
+      code: 200,
+      status: true,
+      message: "Members messaged successfully",
+    };
+
     const { message, recipients } = req.body;
 
     try {
@@ -39,15 +46,21 @@ exports.messages = async (req, res) => {
         const messageInput = await driver.wait(
           until.elementLocated(By.css('div[aria-label="Message"]'))
         );
-        await messageInput.sendKeys(parsedMessage, Key.ENTER);
 
-        console.log(`>>selenium/messages - ${url} success`);
+        if (messageInput) {
+          await messageInput.sendKeys(parsedMessage, Key.ENTER);
 
-        // Wait for a few seconds before sending the next message
-        await driver.sleep(5000);
+          console.log(`>>selenium/messages - ${url} success`);
+
+          // Wait for a few seconds before sending the next message
+          await driver.sleep(5000);
+        }
       }
     } catch (error) {
       console.log(`>>selenium/messages - ${url} error`);
+      response.code = 400;
+      response.status = false;
+      response.message = error.message;
       console.log(error.message);
     } finally {
       // Quit the browser driver when finished
@@ -55,10 +68,7 @@ exports.messages = async (req, res) => {
     }
 
     // Respond with a success message
-    res.json({
-      status: true,
-      message: "Recipients messaged successfully",
-    });
+    res.status(response.code).json(response);
   } else {
     console.log(">>selenium/messages - fb login failed");
     // If login was unsuccessful, respond with an error message
@@ -88,6 +98,12 @@ exports.tagging = async (req, res) => {
 
     const { recipients, message, url } = req.body;
 
+    var response = {
+      code: 200,
+      status: true,
+      message: "Members tagged successfully",
+    };
+
     try {
       // Navigate to the Facebook page or post where the tagging will take place
       console.log(`Processing URL: ${url}`);
@@ -98,35 +114,37 @@ exports.tagging = async (req, res) => {
         until.elementLocated(By.css('div[aria-label="Write a comment"]'))
       );
 
-      await messageInput.sendKeys(`${message} `);
+      if (messageInput) {
+        await messageInput.sendKeys(`${message} `);
 
-      // Loop through the recipients array and tag each username in a separate comment
-      for (const recipient of recipients) {
-        const { username } = recipient;
+        // Loop through the recipients array and tag each username in a separate comment
+        for (const recipient of recipients) {
+          const { username } = recipient;
 
-        // Send the recipient's username preceded by "@" and ENTER keystrokes to tag them in the comment
-        await messageInput.sendKeys(`@${username}`);
-        await driver.sleep(1000);
+          // Send the recipient's username preceded by "@" and ENTER keystrokes to tag them in the comment
+          await messageInput.sendKeys(`@${username}`);
+          await driver.sleep(1000);
+          await messageInput.sendKeys(Key.ENTER);
+          await driver.sleep(1000);
+          await messageInput.sendKeys(" ");
+
+          console.log(`>>selenium/messages - ${username} success`);
+        }
+
+        // Send the message in the comment
         await messageInput.sendKeys(Key.ENTER);
-        await driver.sleep(1000);
-        await messageInput.sendKeys(" ");
-
-        console.log(`>>selenium/messages - ${username} success`);
       }
-
-      // Send the message in the comment
-      await messageInput.sendKeys(Key.ENTER);
     } catch (error) {
       console.log(`>>selenium/messages - ${username} failed`);
+      response.code = 400;
+      response.status = false;
+      response.message = error.message;
       console.log(error.message);
     } finally {
       await driver.quit();
     }
 
-    res.json({
-      status: true,
-      message: "Recipients tagged successfully",
-    });
+    res.status(response.code).json(response);
   } else {
     console.log(">>selenium/tagging - fb login failed");
 
@@ -152,33 +170,141 @@ exports.liking = async (req, res) => {
 
     const { urls } = req.body;
 
+    var response = {
+      code: 200,
+      status: true,
+      message: "Posts liked successfully",
+    };
+
     try {
       for (const url of urls) {
-        console.log(`Processing URL: ${url}`);
-        await driver.get(url);
+        try {
+          console.log(`Processing URL: ${url}`);
+          await driver.get(url);
 
-        const likeButton = await driver.wait(
-          until.elementLocated(By.css('div[aria-label="Like"]'))
-        );
-        await likeButton.click();
+          const removeLike = await driver.findElement(
+            By.css('div[aria-label="Remove Like"]')
+          );
 
-        await driver.sleep(5000);
+          if (removeLike) {
+            console.log("Already liked");
+          }
+        } catch (error) {
+          // this error states that Remove like is not found, so it is available for liking
+          console.log(error.message);
 
-        console.log(`>>selenium/liking - ${url} success`);
+          try {
+            const like = await driver.findElement(
+              By.css('div[aria-label="Like"]')
+            );
+
+            if (like) {
+              await like.click();
+
+              await driver.sleep(5000);
+
+              console.log(`>>selenium/liking - ${url} success`);
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+        }
+
+        // const likeButton = driver.wait(
+        //   until.elementLocated(By.css('div[aria-label="Like"]'))
+        // );
       }
     } catch (error) {
       console.log(`>>selenium/liking - failed`);
+      response.code = 400;
+      response.status = false;
+      response.message = error.message;
       console.log(error.message);
     } finally {
       await driver.quit();
     }
 
-    res.json({
-      status: true,
-      message: "Recipients messaged successfully",
-    });
+    res.status(response.code).json(response);
   } else {
     console.log(">>selenium/liking - fb login failed");
+
+    res.status(400).json({
+      status: false,
+      message: seleniumResponse.message,
+    });
+  }
+};
+
+exports.sharing = async (req, res) => {
+  console.log(">>selenium/sharing");
+
+  const seleniumResponse = await seleniumLogin(
+    res.locals.email,
+    res.locals.password
+  );
+
+  const { status, driver } = seleniumResponse;
+
+  if (status) {
+    console.log(">>selenium/sharing - fb login success");
+
+    const { urls } = req.body;
+
+    var response = {
+      code: 200,
+      status: true,
+      message: "Posts shared successfully",
+    };
+
+    try {
+      for (const url of urls) {
+        console.log(`Processing URL: ${url}`);
+        await driver.get(url);
+
+        const shareButton = driver.wait(
+          until.elementLocated(
+            By.css(
+              'div[aria-label="Send this to friends or post it on your timeline."]'
+            )
+          )
+        );
+
+        if (shareButton) {
+          shareButton.click();
+
+          await driver.sleep(2000);
+
+          try {
+            const shareNow = await driver.wait(
+              until.elementLocated(By.xpath("//*[text()='Share now (Public)']"))
+            );
+
+            if (shareNow) {
+              shareNow.click();
+
+              console.log(">>selenium/sharing - shared successfully");
+
+              await driver.sleep(3000);
+            }
+          } catch (error) {
+            console.log(`>>selenium/sharing - share now not found`);
+            console.log(error.message);
+          }
+        }
+      }
+    } catch (error) {
+      console.log(`>>selenium/sharing - failed`);
+      response.code = 400;
+      response.status = false;
+      response.message = error.message;
+      console.log(error.message);
+    } finally {
+      await driver.quit();
+    }
+
+    res.status(response.code).json(response);
+  } else {
+    console.log(">>selenium/sharing - fb login failed");
 
     res.status(400).json({
       status: false,
